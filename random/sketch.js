7ab53071fb91ea7d35a606218304a0542adad574
bluevvaves <160423864+bluevvaves@users.noreply.github.com>
2024-06-19 17:52:26 +0300

    Semi-large update
    Added updates to the circles and lines, and the portfolio now has some barebones code. Is functional on desktop, non-functional on other platforms.

--- random/sketch.js
+++ random/sketch.js
@@ -8,20 +8,18 @@ let amt, startColor, newColor;
 
 function setup() {
 let canvas = createCanvas(windowWidth, windowHeight);
-canvas.parent('canvas-container');
     col.r= random(0,255);
   col.g= random(0,255);
   col.b= random(0,255);
 background(col.r,col.g,col.b);
 }
 
-
 function mousePressed() {
 col.r= random(0,255);
   col.g= random(0,255);
   col.b= random(0,255);
   startColor = newColor;
-  newColor = color(random(255),random(255),random(255));
+  newColor = color(random(0,255));
 }
 
 function keyPressed() {
@@ -32,15 +30,18 @@ if (keyCode === ENTER) {
 
 
 function draw() {
-background(col.r,col.g,col.b, 10);
+background(random(0,225), 10);
   noFill();
-  stroke(0,20,50,100);
-  circle(random(450), random(300),random(500));
-   strokeWeight(random(2));
-  translate(random(70), random(70));
-  circle(150, 500, random, 30, random(750), 500);
-   circle(23, random, 110, random(35), 640, random(60));
-   circle(640, 500, random(403), 975, 940, random);
-     circle(random, random, 730, random(230), 940, 450);
+  stroke(225);
+  
+  circle(random(windowHeight), random(windowWidth), random(30,1000));
+  // let x = random(45, 55);
+  // let y = random(45, 55);
+  //  strokeWeight(random(2));
+  // translate(random(70), random(70));
+  // circle(150, 500, random, 30, random(750), 500);
+  //  circle(23, random, 110, random(35), 640, random(60));
+  //  circle(640, 500, random(403), 975, 940, random);
+  //    circle(random(10,70), random, 730, random(230), 940, 450);
 }
 
